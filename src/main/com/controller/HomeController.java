afcb46890c9c82365854a75457cdd2a7d0f750a7
nan <nanglaokhay2015@gmail.com>
2020-12-19 00:27:25 +0900

    dispatcher servlet and request mapping

--- src/main/com/controller/HomeController.java
+++ src/main/com/controller/HomeController.java
@@ -3,7 +3,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
        
 @Controller
-public class AddController {
+public class HomeController {
 	@RequestMapping("/")
     public String welcome(Model model) {
          model.addAttribute("greeting", "Welcome to Web Store!");
